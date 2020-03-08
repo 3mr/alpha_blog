37bab7a259e60a1853b12682d19cdc0e3633d2e2
Amr Albashrawi <aalbashrawi@winfooz.com>
2020-03-08 11:54:35 +0200

    Add accossiation between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
+  # accossiations
+  belongs_to :user
+
   # validation
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  # accossiations
+  has_many :artiles
+
+  # callbacks
+  before_save { self.email = email.downcase }
+
   # constants
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   # validation
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
